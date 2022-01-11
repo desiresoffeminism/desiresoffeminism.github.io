cecba55ff01bfd66ac5305b5809e387a19d72df3
emucc <github@elrimer.com>
2022-01-11 15:54:19 +0000

    add timeline section

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,7 +1,20 @@
+// TIMELINE DATA
 $(document).ready(function () {
   $.getJSON("data/timeline.json", function (data) {
-    timeline_data = data; // <- change or add functioning code within here
-    // also fit in JSON.parse(timeline_data)
-    // maybe as timeline_data = JSON.parse(data)? if that even works
+    timeline_data = JSON.parse(data);
+    listItemString = $("listItem").html();
+
+    timeline_data.forEach(buildNewList);
+
+    function buildNewList(item) {
+      var listItem = $("<li>" + listItemString + "</li>");
+      var listItemTitle = $(".title", listItem);
+      listItemTitle.html(item.title);
+      var listItemDate = $(".main_date", listItem);
+      listItemDate.html(item.main_date);
+      var listItemDesc = $(".description", listItem);
+      listItemDesc.html(item.description);
+      $("#dataList").append(listItem);
+    }
   });
 });
