1f517a5b096aa83df5621d755a0e11b90b5a9aa2
emucc <github@elrimer.com>
2022-01-12 10:40:28 +0000

    connected JSON to JS to HTML

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,20 +1,22 @@
 // TIMELINE DATA
 $(document).ready(function () {
-  $.getJSON("data/timeline.json", function (data) {
-    timeline_data = JSON.parse(data);
-    listItemString = $("listItem").html();
+  console.log("Document ready");
+});
 
-    timeline_data.forEach(buildNewList);
+$.getJSON("data/timeline.json", function (data) {
+  timeline_data = data;
+  listItemString = $("#listItem").html();
 
-    function buildNewList(item) {
-      var listItem = $("<li>" + listItemString + "</li>");
-      var listItemTitle = $(".title", listItem);
-      listItemTitle.html(item.title);
-      var listItemDate = $(".main_date", listItem);
-      listItemDate.html(item.main_date);
-      var listItemDesc = $(".description", listItem);
-      listItemDesc.html(item.description);
-      $("#dataList").append(listItem);
-    }
-  });
+  timeline_data.forEach(buildNewList);
+
+  function buildNewList(item) {
+    var listItem = $("<div>" + listItemString + "</div>");
+    var listItemTitle = $(".title", listItem);
+    listItemTitle.html(item.title);
+    var listItemDate = $(".main_date", listItem);
+    listItemDate.html(item.main_date);
+    var listItemDesc = $(".description", listItem);
+    listItemDesc.html(item.description);
+    $("#dataList").append(listItem);
+  }
 });
