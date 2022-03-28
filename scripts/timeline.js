99714b37c3234eb043873522ba0dcb298e7dc767
emucc <github@elrimer.com>
2022-03-28 16:52:55 +0100

    initial filter functionality

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,16 +1,29 @@
-// TIMELINE DATA
 $(document).ready(function () {
   console.log("Document ready");
 });
 
+// ----- TIMELINE DATA -----
 // READ AND DISPLAY TIMELINE DATA
 // inspired by Anant Anand Gupta
 $.getJSON("data/timeline.json", function (data) {
   timeline_data = data;
   listItemString = $(".list-item").html();
   emptyArray = [""];
+  countries_list = [];
+  topics_list = [];
 
+  // build timeline
   timeline_data.forEach(buildNewList);
+
+  // build filter dropdowns
+  timeline_data.forEach(fetchSearchTags);
+
+  // TODO: display full country names with country codes
+  countries_list.sort();
+  topics_list.sort();
+  $("#countryDrop").append(dropdownHTML(countries_list));
+  $("#topicrop").append(dropdownHTML(topics_list));
+
 
   // filling in item info from JSON data
   function buildNewList(item) {
@@ -100,8 +113,78 @@ $.getJSON("data/timeline.json", function (data) {
 
     return li_string;
   }
+
+  // getting unique country codes & topics
+  function fetchSearchTags(item) {
+    if (item.country != emptyArray) {
+      split_country = item.country.split(', ');
+
+      split_country.forEach(function (i) {
+        if (!countries_list.includes(i)) {
+          countries_list.push(i);
+        }
+      })
+    }
+
+    if (item.topic != emptyArray) {
+      split_topic = item.topic.split(', ');
+
+      split_topic.forEach(function (i) {
+        if (!topics_list.includes(i)) {
+          topics_list.push(i);
+        }
+      })
+    }
+  }
+
+  // transforms dropdown contents to <li> elements 
+  function dropdownHTML(array) {
+    li_string = "";
+
+    li_array = array.map(x => "<li class=\"dropdown-item\">" + x + "</li>");
+    li_array.forEach(function (i) {
+      li_string += i;
+    })
+
+    return li_string;
+  }
 });
 
+// ----- SITE FUNCTIONALITY -----
+// NAV DROPDOWN SEARCH
+function cDropdownSearch() {
+  var input, filter, search_container, search_item, country, i;
+  input = document.getElementById("countrySearch");
+  filter = input.value.toUpperCase();
+  search_container = document.getElementById("countryDrop");
+  search_item = search_container.getElementsByClassName("dropdown-item");
+
+  for (i = 0; i < search_item.length; i++) {
+    country = search_item[i];
+    if (country.innerText.toUpperCase().indexOf(filter) > -1) {
+      search_item[i].style.display = "";
+    } else {
+      search_item[i].style.display = "none";
+    }
+  }
+}
+
+function tDropdownSearch() {
+  var input, filter, search_container, search_item, topic, i;
+  input = document.getElementById("topicSearch");
+  filter = input.value.toUpperCase();
+  search_container = document.getElementById("topicDrop");
+  search_item = search_container.getElementsByClassName("dropdown-item");
+
+  for (i = 0; i < search_item.length; i++) {
+    topic = search_item[i];
+    if (topic.innerText.toUpperCase().indexOf(filter) > -1) {
+      search_item[i].style.display = "";
+    } else {
+      search_item[i].style.display = "none";
+    }
+  }
+}
 
 // HORIZONTAL SCROLL
 const scrollContainer = document.getElementById("timeline");
@@ -123,9 +206,8 @@ $(document).mousemove(function () {
 
 // EXPAND TIMELINE ITEM
 // inspired by Nicole Oakes
+// TODO: add method of closing info box by clicking off of it
 $(document).on('click', '.timeline-icon', function () {
   $(this).toggleClass("expand");
   $(this).next().next().toggleClass('expand');
 });
-
-// TODO: add method of closing info box by clicking off of it
